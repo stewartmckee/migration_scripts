80b43797621a5b757c931a28bee95110c7349e53
Stewart McKee <stewart@theizone.co.uk>
2013-10-27 00:29:19 +0100

    updated to conditionaly set title

--- set_title.rb
+++ set_title.rb
@@ -1,3 +1,5 @@
 # sets the title tag to first h1 tag
 
-document.at("title").content = document.at("h1").content
+if document.at("title").nil?
+  document.at("title").content = document.at("h1").content
+end
